07bd37186f6083c2829b5e9a8a23746737e85715
claraj <clarajchu@gmail.com>
2021-01-16 18:04:32 -0500

    added ball

--- ball.js
+++ ball.js
@@ -1,24 +1,55 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0x262336);
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
+var light1 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light1);
 
-camera.position.z = 5;
+
+//MATERIALS
+var material = new THREE.MeshPhongMaterial({
+color:0xff4646,
+emissive: 0xff9888,
+side: THREE.DoubleSide,
+specular:0xffffff,
+shininess:120,
+});
+
+
+
+
+var geometry = new THREE.SphereGeometry( 7, 32, 32);
+var sphere = new THREE.Mesh( geometry, material );
+sphere.position.set(0,0,-10)
+sphere.castShadow = true;
+scene.add( sphere );
+
+camera.position.z = 30;
+
+window.addEventListener('resize', onWindowResize, false );
 
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	sphere.rotation.x += 0.01;
+	sphere.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 };
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 animate();
